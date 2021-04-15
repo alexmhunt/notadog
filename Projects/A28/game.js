360f3c1b40d71eeabcad13a65699d2a9fd53a7c9
Oliver Rayner <orayner@wpi.edu>
2021-04-15 06:53:36 -0400

    Volume Adjusted
    Refined prototype is finished.

--- Projects/A28/game.js
+++ Projects/A28/game.js
@@ -647,7 +647,7 @@ const G = ( function () {
 				}
 				PS.dbEvent( TEAM, "startup", user );
 				PS.dbSend( TEAM, PS.CURRENT, { discard : true } );
-			}, { active : false } );
+			}, { active : true } );
 
 			// Change the false in the final line above to true
 			// before deploying the code to your Web site.
@@ -683,17 +683,17 @@ const G = ( function () {
 					if(checkSolved()){
 						switch(currentLevel){
 							case 0:
-								PS.audioPlay("fx_ding",{volume:.25});
+								PS.audioPlay("fx_ding",{volume:.2});
 								PS.statusText("There! Moving on...");
 								nextLevel();	
 								break;
 							case 1:
-								PS.audioPlay("fx_ding",{volume:.25});
+								PS.audioPlay("fx_ding",{volume:.2});
 								PS.statusText("One step closer to home!");
 								nextLevel();	
 								break;
 							case 2:
-								PS.audioPlay("fx_ding",{volume:.25});
+								PS.audioPlay("fx_ding",{volume:.2});
 								PS.statusText("Will I ever get out of here?");
 								break;
 							default:
